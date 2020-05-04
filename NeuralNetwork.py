fixed regularization parameter and edited backpropagation function to be vectorized

NeuralNetwork.py
@@ -19,18 +19,16 @@ def activate(x, fn='sigmoid', grad=False):
             return gradReLu(x)
 
 
-
-
 def featureScale(x):
     return (x - x.mean()) / x.std()
 
 
 def gradReLu(x):
-    y=1 if x>0 else 0
+    y = 1 if x > 0 else 0
 
 
 def reLu(x):
-    y=x if x>0 else 0
+    return max(0,x)
 
 
 def sigmoid(x):
@@ -42,81 +40,109 @@ def gradSigmoid(x):
     return x * (1 - x)
 
 
+class neural_layer:
+    def __init__(self):
+        self.units=0
+        self.activation='sigmoid'
+        self.layer=0
 class NeuralNetwork:
     def __init__(self):
         self.activations = []
-        self.hiddenLayers = 0
-        self.hiddenUnits = 0
+        self.layers = 0
         self.inputUnits = 0
-        self.outputUnits = 0
         self.network = []
         np.random.seed(1)
-        self.activation='sigmoid'
+        self.lastOutput = 0
+        self.added_input = 0
+        self.added_output = 0
+        self.trainSize = 0
         return
+
+    def add_input_layer(self, inputUnits):
+        if not self.added_input:
+            self.inputUnits = inputUnits + 1
+            self.lastOutput = inputUnits
+            self.activations.append(0)
+            self.added_input = 1
+        else:
+            print("input layer already added")
+
+    def add_hidden_layers(self, hiddenUnits, hiddenLayers=1,activation='sigmoid'):
+        if self.added_input:
+            for i in range(hiddenLayers):
+                layer=neural_layer()
+                layer.units=hiddenUnits
+                layer.activation=activation
+                layer.layer=6 * np.random.rand(hiddenUnits, self.lastOutput + 1) - 3
+                self.network.append(layer)
+                self.activations.append(0)
+                self.lastOutput = hiddenUnits
+            self.layers += hiddenLayers
+        else:
+            print("add input layer first")
+
+    def add_output_layer(self, outputUnits,activation='sigmoid'):
+        if not self.added_output and self.added_input:
+            layer=neural_layer()
+            layer.layer=6 * np.random.rand(outputUnits, self.lastOutput + 1) - 3
+            layer.activation=activation
+            layer.units=outputUnits
+            self.activations.append(0)
+            self.network.append(layer)
+            self.added_output = 1
+            self.layers+=1
+        else:
+            if self.added_output:
+                print("output layer already added")
+            else:
+                print("add input layer first")
 
     def __forwardPropagate(self, X):
         m, n = np.shape(X)
         self.activations[0] = X
-        for i in range(1, self.hiddenLayers + 2):
+        for i in range(1, self.layers+1):
             if i != 1:
                 self.activations[i - 1] = np.append(np.ones((m, 1)), self.activations[i - 1], axis=1)
-            self.activations[i] = activate(self.activations[i - 1].dot(self.network[i-1].T),self.activation)
+            self.activations[i] = activate(self.activations[i - 1].dot(self.network[i - 1].layer.T), self.network[i-1].activation)
 
     def __backpropagate(self, y, Lambda):
-        deltaNetwork = [0] * (self.hiddenLayers + 1)
-        deltas = [0] * (self.hiddenLayers + 1)
-        deltas[-1] = (self.activations[-1] - y) * activate(self.activations[-1],self.activation,True)
-        for i in reversed(range(self.hiddenLayers)):
-            deltas[i] = np.dot(deltas[i + 1], self.network[i + 1]) * activate(self.activations[i + 1],self.activation,True)
+        deltaNetwork = [0] * (self.layers)
+        deltas = [0] * (self.layers)
+        deltas[-1] = (self.activations[-1] - y) * activate(self.activations[-1], self.network[-1].activation, True)
+        for i in reversed(range(self.layers-1)):
+            deltas[i] = np.dot(deltas[i + 1], self.network[i + 1].layer) * activate(self.activations[i + 1], self.network[i+1].activation,
+                                                                              True)
             deltas[i] = deltas[i][:, 1:]
-        for i in range(self.hiddenLayers+1):
+        for i in range(self.layers):
             deltaNetwork[i] = deltas[i].T.dot(self.activations[i])
-        error = (-1 / self.m) * (np.sum(y * np.log(self.activations[-1]) + (1 - y) * np.log(1 - self.activations[-1])))
+        error = (-1 / self.trainSize) * (
+            np.sum(y * np.log(self.activations[-1]) + (1 - y) * np.log(1 - self.activations[-1])))
         if Lambda != 0:
             regTerm = 0
             regNetwork = self.network
-            for i in range(self.hiddenLayers + 1):
-                regNetwork[i][:, 0] = 0
-                deltaNetwork[i] += regNetwork[i] * Lambda
-                regTerm += np.sum(np.square(regNetwork[i][:, 1:]))
-                error += (Lambda / (2 * self.m)) * regTerm
-        for i in range(self.hiddenLayers + 1):
-            deltaNetwork[i] /= self.m
+            for i in range(self.layers):
+                regNetwork[i].layer[:, 0] = 0
+                deltaNetwork[i] += regNetwork[i].layer * Lambda
+                regTerm += np.sum(np.square(regNetwork[i].layer[:, 1:]))
+                error += (Lambda / (2 * self.trainSize)) * regTerm
+        for i in range(self.layers):
+            deltaNetwork[i] /= self.trainSize
         return deltaNetwork, error
 
-    def __initializeNetwork(self):
-        self.activations=[0]*(self.hiddenLayers+2)
-        inputToHiddenTheta = 6 * np.random.rand(self.hiddenUnits, self.inputUnits) - 3
-        self.network.append(inputToHiddenTheta)
-        hiddenTheta = [None] * (self.hiddenLayers - 1)
-        for i in range(self.hiddenLayers - 1):
-            hiddenTheta[i] = 6 * np.random.rand(self.hiddenUnits, self.hiddenUnits + 1) - 3
-        self.network += hiddenTheta
-        hiddenToOutputTheta = 6 * np.random.rand(self.outputUnits, self.hiddenUnits + 1) - 3
-        self.network.append(hiddenToOutputTheta)
-        return
-
-    def train(self, X, y, hiddenUnits, alpha=0.01, iters=10000, Lambda=0, hiddenLayers=1,activation='sigmoid'):
-        self.activation=activation
+    def train(self, X, y, alpha=0.01, iters=10000, Lambda=0.01):
         y = np.array(y)
-        self.m, n = np.shape(X)
-        X = np.append(np.ones((self.m, 1)), X, axis=1)
-        m,self.outputUnits=np.shape(y)
-        n += 1
-        self.inputUnits = n
-        self.hiddenUnits = hiddenUnits
-        self.hiddenLayers = hiddenLayers
+        self.trainSize, n = np.shape(X)
+        X = np.append(np.ones((self.trainSize, 1)), X, axis=1)
         # initialize network
-        self.__initializeNetwork()
         stage = iters / 100
         Jvec = np.zeros(iters)
         for iter in range(iters):
             self.__forwardPropagate(X)
             deltaNetwork, J = self.__backpropagate(y, Lambda)
-            for j in range(self.hiddenLayers + 1):
-                self.network[j] -= alpha * deltaNetwork[j]
-            if iter%stage==0:
-                print("error: ",J)
+            for j in range(self.layers):
+                self.network[j].layer -= alpha * deltaNetwork[j]
+            if iter % stage == 0:
+                print("error: ", J)
             Jvec[iter] = J
         return Jvec
 

main.py
@@ -4,22 +4,26 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
-data = pd.read_csv('diabetes.csv')
+data = pd.read_csv('data_banknote_authentication.csv')
 X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
 X = featureScale(X)
 m = np.shape(y)
 y = np.reshape(y, (m[0], 1))
-
+m,n=np.shape(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
 
 model = NeuralNetwork()
+model.add_input_layer(n)
+model.add_hidden_layers(10,3)
+model.add_output_layer(1)
 
-j = model.train(X_train, y_train, hiddenUnits=12, hiddenLayers=5)
+j = model.train(X_train, y_train, alpha=0.03,iters=10000,Lambda=0.1)
 y_pred = model.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy = (cm[0, 0] + cm[1, 1]) / sum(sum(cm))
+
 print("accuracy: ", accuracy)
 plt.plot(j)
 plt.show()